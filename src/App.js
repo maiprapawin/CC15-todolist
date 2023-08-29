2044f4111d80186f8ee7981a9c66dd4bdf889c92
maiprapawin <maiprapawin@gmail.com>
2023-08-29 16:55:46 +0700

    refactor:searchbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.scss";
-import { FaHome } from "react-icons/fa";
 import Header from "./Header";
 
 function App() {
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,6 @@
 import "./Header.scss";
-import { FaHome, FaSearch } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+import Search from "./Search"; // ใช้ชื่อเดียวกันกับที่เราตั้งไว้ใน export หรือจะตั้งชื่ออื่นก็ได้ ตรงคำว่า Search
 
 function Header() {
   return (
@@ -11,12 +12,7 @@ function Header() {
         <h1>TodoList</h1>
       </div>
       <div className="header__search">
-        <div className="search">
-          <span className="search__icon">
-            <FaSearch />
-          </span>
-          <input type="text" placeholder="search" className="search__input" />
-        </div>
+        <Search />
       </div>
     </header>
   );
