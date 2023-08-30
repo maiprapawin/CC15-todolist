532b0b65bdbce37b5dd36efee436e2f05fea2393
maiprapawin <maiprapawin@gmail.com>
2023-08-30 12:12:29 +0700

    refactor: create ListItem component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,6 +6,7 @@ import {
   FaChevronDown,
 } from "react-icons/fa";
 import Header from "../components/Header";
+import ListItem from "../components/ListItem";
 
 function App() {
   return (
@@ -18,18 +19,20 @@ function App() {
         <aside className="sidebar">
           <section className="sidebar__category">
             <ul className="list">
-              <li className="list__item">
-                <FaInbox className="list__item__icon" />
-                <p className="list__item__text">Inbox</p>
-              </li>
-              <li className="list__item">
-                <FaCalendar className="list__item__icon" />
-                <p className="list__item__text">Today</p>
-              </li>
-              <li className="list__item">
-                <FaCalendarAlt className="list__item__icon" />
-                <p className="list__item__text">Next 7 Days</p>
-              </li>
+              <ListItem
+                text="Inbox"
+                icon={<FaInbox className="list__item__icon" />}
+              />
+
+              <ListItem
+                text="Today"
+                icon={<FaCalendar className="list__item__icon" />}
+              />
+
+              <ListItem
+                text="Next 7 Days"
+                icon={<FaCalendarAlt className="list__item__icon" />}
+              />
             </ul>
           </section>
           <section className="sidebar__category"></section>
