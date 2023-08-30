361dec0211855ef3cb85c2c3c366f8b439271262
maiprapawin <maiprapawin@gmail.com>
2023-08-30 14:21:32 +0700

    finished: accordion

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -19,23 +19,34 @@ function App() {
         <aside className="sidebar">
           <section className="sidebar__category">
             <ul className="list">
-              <ListItem
-                text="Inbox"
-                icon={<FaInbox className="list__item__icon" />}
-              />
+              <ListItem text="Inbox" icon={<FaInbox />} active={true} />
 
-              <ListItem
-                text="Today"
-                icon={<FaCalendar className="list__item__icon" />}
-              />
+              <ListItem text="Today" icon={<FaCalendar />} active={false} />
 
               <ListItem
                 text="Next 7 Days"
-                icon={<FaCalendarAlt className="list__item__icon" />}
+                icon={<FaCalendarAlt />}
+                active={false}
               />
             </ul>
           </section>
-          <section className="sidebar__category"></section>
+          <section className="sidebar__category">
+            <div className="accordion">
+              {/* Toggle */}
+              <div className="accordion__toggle">
+                <li className="accordion__item">
+                  <FaChevronDown className="accordion__item__icon accordion__item__active" />
+                  <p className="accordion__item__text">Projects</p>
+                </li>
+              </div>
+
+              {/* Lists */}
+              <ul className="list">
+                <ListItem text="Project-A" icon={<FaInbox />} active={true} />
+                <ListItem text="Project-B" icon={<FaInbox />} active={false} />
+              </ul>
+            </div>
+          </section>
         </aside>
       </div>
       <div className="todo__content">TodoContent</div>
